66f5f3a4d15c319d9de5c04cbb91e6293675e9ac
Christopher Lee <hanernlee@gmail.com>
2017-10-11 00:17:23 +1100

    Chapter 3 and 4

---
+++ chapter03-views & chapter04-delegation/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Christopher Lee on 10/10/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    // using Closure to instantiate numberformatter
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+    }
+    
+    // iOS design pattern using delegation
+    // text field calls method on it's delegate to check if replacement string should be accepted or rejected
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Only allow digits Bronze Challenge
+        let allowedCharacterSet = CharacterSet(charactersIn: "0123456789.-")
+        let replacementStringCharacterSet = CharacterSet(charactersIn: string)
+        if !replacementStringCharacterSet.isSubset(of: allowedCharacterSet) {
+            print("Rejected (Invalid Character)")
+            return false
+        }
+        
+        // Check for multiple .
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeparator != nil, replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+}
