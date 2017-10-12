0632dba63a8c3bd0801e7cadc370541558196af9
Christopher Lee <hanernlee@gmail.com>
2017-10-13 01:16:16 +1100

    Rename folder

---
+++ chapter03-views__chapter04-delegation __chapter05-view__chapter06-programatically__chapter07-localization/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Christopher Lee on 10/10/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    // using Closure to instantiate numberformatter
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ConversionViewController loaded its view")
+        updateCelsiusLabel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated) // No need for semicolon
+        
+        let hour = Calendar.current.component(.hour, from: Date())
+        var bgColor: UIColor
+        
+        switch hour {
+        case 6..<12:
+            bgColor = UIColor.cyan
+        case 12..<17:
+            bgColor = UIColor.magenta
+        case 17..<22:
+            bgColor = UIColor.purple
+        default:
+            bgColor = UIColor.lightGray
+        }
+         self.view.backgroundColor = bgColor
+    }
+    
+    // iOS design pattern using delegation
+    // text field calls method on it's delegate to check if replacement string should be accepted or rejected
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Only allow digits Bronze Challenge
+        let allowedCharacterSet = CharacterSet(charactersIn: "0123456789,-")
+        let replacementStringCharacterSet = CharacterSet(charactersIn: string)
+        if !replacementStringCharacterSet.isSubset(of: allowedCharacterSet) {
+            print("Rejected (Invalid Character)")
+            return false
+        }
+        
+        // Check for multiple .
+//        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+//        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        // Change to use locale-specific decimal
+        let currentLocale = Locale.current
+        let decimalSeparator = currentLocale.decimalSeparator ?? "."
+        
+        let existingTextHasDecimalSeparator = textField.text?.range(of: decimalSeparator)
+        let replacementTextHasDecimalSeparator = string.range(of: decimalSeparator)
+        
+        if existingTextHasDecimalSeparator != nil, replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let number = numberFormatter.number(from: text) {
+            fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+}
---
+++ chapter03-views__chapter04-delegation __chapter05-view__chapter06-programatically__chapter07-localization/WorldTrotter/WorldTrotter/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  WorldTrotter
+//
+//  Created by Christopher Lee on 11/10/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+// Bronze Challenge
+class WebViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string: "https://www.bignerdranch.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
