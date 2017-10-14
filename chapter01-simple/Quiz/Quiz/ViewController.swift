55d16b9aae36e5188d0743f4a551bade5e4cb053
Christopher Lee <hanernlee@gmail.com>
2017-10-14 19:57:17 +1100

    Completing animation challenges spring animations

--- chapter01-simple/Quiz/Quiz/ViewController.swift
+++ chapter01-simple/Quiz/Quiz/ViewController.swift
@@ -59,16 +59,23 @@ class ViewController: UIViewController {
         self.nextQuestionLabelCenterXConstraint.constant = 0
         self.currentQuestionLabelCenterXConstraint.constant += screenWidth
         
-        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
-            self.currentQuestionLabel.alpha = 0
-            self.nextQuestionLabel.alpha = 1
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.5,
+            initialSpringVelocity: 0.5,
+            options: [.curveLinear],
+            animations: {
+                self.currentQuestionLabel.alpha = 0
+                self.nextQuestionLabel.alpha = 1
             
-            self.view.layoutIfNeeded()
-        }, completion: { _ in
-            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
-            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
-            
-            self.updateOffScreenLabel()
+                self.view.layoutIfNeeded()
+            },
+            completion: { _ in
+                swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                
+                self.updateOffScreenLabel()
         })
     }
     
