cdca5a478fc2f041644d8ea144f5b767300a77e1
Christopher Lee <hanernlee@gmail.com>
2017-10-15 19:08:53 +1100

    Chapter 12 Challenges

---
+++ chapter11-editing-uitableview__chapter12-subclass-uitableviewcell/Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,30 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Christopher Lee on 15/10/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    // Bronze Challenge
+    var isUnderFifty: Bool = false {
+        didSet {
+            valueLabel.textColor = isUnderFifty ? UIColor.green : UIColor.red
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+}
---
+++ chapter11-editing-uitableview__chapter12-subclass-uitableviewcell/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Christopher Lee on 15/10/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    var itemStore: ItemStore!
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        // Create new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you are currently in editing mode
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom:0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Adding one more for no elements string
+        return itemStore.allItems.count + 1
+    }
+    
+    // Viewing
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row < itemStore.allItems.count {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",for: indexPath )
+
+//            cell.textLabel?.text = item.name
+//            cell.detailTextLabel?.text = "$" + String(item.valueInDollars)
+            
+            // Set the text on the cell with the description of the item that is at the nth index of items
+            // where n = row this cell will appear in on table view
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+            let item = itemStore.allItems[indexPath.row]
+            
+            // Configure cell with the item
+            cell.nameLabel.text = item.name
+            cell.serialNumberLabel.text = item.serialNumber
+            cell.valueLabel.text = "$\(item.valueInDollars)"
+            
+            // Bronze Challenge
+            cell.isUnderFifty = (item.valueInDollars <= 50)
+            
+            return cell;
+        } else {
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell_NMI")
+            
+            cell.textLabel?.text = "No more Items!"
+            
+            return cell
+        }
+    }
+    
+    // Check if movement is within item count
+    override func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        if (proposedDestinationIndexPath.row >= itemStore.allItems.count) {
+            return sourceIndexPath
+        }
+        return proposedDestinationIndexPath
+    }
+    
+    // Check if editable
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if (indexPath.row >= itemStore.allItems.count){
+            return false
+        }
+        return true
+    }
+    
+    // Moving items
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    // Deleting
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Remove"
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // If table view is asking to commit a delete command
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            // User confirmation
+            let title = "Remove \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Remove", style: .destructive, handler: { (action) -> Void in
+                // Remove the item from the store
+                self.itemStore.removeItem(item)
+                
+                // Also remove that row from table view with animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+        }
+    }
+}
