b64b6e809544408d773a02712ce8ff391277b952
Christopher Lee <hanernlee@gmail.com>
2017-10-16 00:18:14 +1100

    Chapter 13

--- chapter11-editing-uitableview__chapter12-subclass-uitableviewcell__hpater13-stackviews/Homepwner/Homepwner/ItemsViewController.swift
+++ chapter11-editing-uitableview__chapter12-subclass-uitableviewcell__hpater13-stackviews/Homepwner/Homepwner/ItemsViewController.swift
@@ -59,6 +59,23 @@ class ItemsViewController: UITableViewController {
         return itemStore.allItems.count + 1
     }
     
+    // Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // if the triggered segue is the "showItem" segue
+        switch segue.identifier {
+        case "showItem"?:
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier")
+        }
+    }
+    
     // Viewing
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row < itemStore.allItems.count {
