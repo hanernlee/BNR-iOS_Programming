63170e8081b5d147c236e614d820efed50c311d0
Christopher Lee <hanernlee@gmail.com>
2017-10-19 00:08:34 +1100

    Chapter 18 touch events

---
+++ chapter18-touchEvents/TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Christopher Lee on 18/10/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
