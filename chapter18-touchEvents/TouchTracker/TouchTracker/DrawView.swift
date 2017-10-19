f7bb5192bd3cd3357158bd6ef3627c8d36d2d062
Christopher Lee <hanernlee@gmail.com>
2017-10-19 23:48:15 +1100

    Chapter 18 challenges

--- chapter18-touchEvents/TouchTracker/TouchTracker/DrawView.swift
+++ chapter18-touchEvents/TouchTracker/TouchTracker/DrawView.swift
@@ -12,6 +12,9 @@ class DrawView: UIView {
     var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
     
+    var currentCircle = Circle()
+    var finishedCircles = [Circle]()
+    
     @IBInspectable var finishedLineColor: UIColor = UIColor.black {
         didSet {
             setNeedsDisplay()
@@ -60,6 +63,16 @@ class DrawView: UIView {
         for (_ , line) in currentLines {
             stroke(line)
         }
+        
+        // Draw Circles
+        finishedLineColor.setStroke()
+        for circle in finishedCircles {
+            let path = UIBezierPath(ovalIn: circle.rect)
+            path.lineWidth = lineThickness
+            path.stroke()
+        }
+        currentLineColor.setStroke()
+        UIBezierPath(ovalIn: currentCircle.rect).stroke()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -71,12 +84,19 @@ class DrawView: UIView {
         // Log statement to see the order of events
         print(#function)
         
-        for touch in touches {
-            let location = touch.location(in: self)
-            let newLine = Line(begin: location, end: location)
-            
-            let key = NSValue(nonretainedObject: touch)
-            currentLines[key] = newLine
+        if touches.count == 2 {
+            let touchesArray = Array(touches)
+            let location1 = touchesArray[0].location(in: self)
+            let location2 = touchesArray[1].location(in: self)
+            currentCircle = Circle(point1: location1, point2: location2)
+        } else {
+            for touch in touches {
+                let location = touch.location(in: self)
+                let newLine = Line(begin: location, end: location)
+                
+                let key = NSValue(nonretainedObject: touch)
+                currentLines[key] = newLine
+            }
         }
         
         setNeedsDisplay()
@@ -90,9 +110,16 @@ class DrawView: UIView {
         // Log statement to see order of events
         print(#function)
         
-        for touch in touches {
-            let key = NSValue(nonretainedObject: touch)
-            currentLines[key]?.end = touch.location(in: self)
+        if touches.count == 2 {
+            let touchesArray = Array(touches)
+            let location1 = touchesArray[0].location(in: self)
+            let location2 = touchesArray[1].location(in: self)
+            currentCircle = Circle(point1: location1, point2: location2)
+        } else {
+            for touch in touches {
+                let key = NSValue(nonretainedObject: touch)
+                currentLines[key]?.end = touch.location(in: self)
+            }
         }
         
         setNeedsDisplay()
@@ -111,14 +138,23 @@ class DrawView: UIView {
         // Log statement to see order of events
         print(#function)
         
-        for touch in touches {
-            let key = NSValue(nonretainedObject: touch)
-            
-            if var line = currentLines[key] {
-                line.end = touch.location(in: self)
+        if touches.count == 2 {
+            let touchesArray = Array(touches)
+            let location1 = touchesArray[0].location(in: self)
+            let location2 = touchesArray[1].location(in: self)
+            currentCircle = Circle(point1: location1, point2: location2)
+            finishedCircles.append(currentCircle)
+            currentCircle = Circle()
+        } else {
+            for touch in touches {
+                let key = NSValue(nonretainedObject: touch)
                 
-                finishedLines.append(line)
-                currentLines.removeValue(forKey: key)
+                if var line = currentLines[key] {
+                    line.end = touch.location(in: self)
+                    
+                    finishedLines.append(line)
+                    currentLines.removeValue(forKey: key)
+                }
             }
         }
         
@@ -130,6 +166,7 @@ class DrawView: UIView {
         print(#function)
         
         currentLines.removeAll()
+        currentCircle = Circle()
         
         setNeedsDisplay()
     }
--- chapter18-touchEvents/TouchTracker/TouchTracker/Line.swift
+++ chapter18-touchEvents/TouchTracker/TouchTracker/Line.swift
@@ -6,10 +6,36 @@
 //  Copyright © 2017 Christopher Lee. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreGraphics
 
 struct Line {
     var begin = CGPoint.zero
     var end = CGPoint.zero
+}
+
+extension Line {
+    var angleDegree : CGFloat {
+        
+        guard begin != end else { return 0 }
+        
+        let dX = end.x - begin.x
+        let dY = end.y - begin.y
+        
+        var angle = atan2(dY, dX) * 180 / CGFloat(Double.pi)
+        
+        //make negative angles be positive and angles can go from 0 to 360
+        if angle < 0 {
+            angle = angle + 360
+        }
+        print(angle)
+        return CGFloat(angle)
+        
+    }
+    
+    var color: UIColor {
+        let hueCode = angleDegree / 360
+        
+        return UIColor(hue: hueCode, saturation: 1, brightness: 1, alpha: 0.6)
+    }
 }
